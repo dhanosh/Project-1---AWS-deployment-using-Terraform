f3acf8f509fd221160b58909b82bcaf1dabaf264
root <root@Dhannat-PC.localdomain>
2020-06-20 00:07:33 +0530

    added web server subnet

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 variable "region" {
-	default = "us-west-1"
+  default = "us-west-1"
 }
 
 variable "access_key" {}
@@ -7,12 +7,26 @@ variable "access_key" {}
 variable "secret_key" {}
 
 variable "vpc_cidr_block" {
-	default = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "vpc_tenancy" {
-	default = "default"
+  default = "default"
 }
 
+variable "az" {
+  type    = list(string)
+  default = ["us-west-1b", "us-west-1c"]
+}
+
+variable "pub_subnet_cidr" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "priv-web-sub-cidr" {
+  type    = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
 
 
